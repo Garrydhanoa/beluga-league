4a76bd27cfb41ec0507e7465167c5bd5df2e9549
Garrydhanoa <garryd2@educbe.ca>
2025-07-22 15:30:29 -0600

    Refactor configuration files; update next.config.js for conditional output, enhance package.json scripts, and add vercel.json for deployment settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
+    domains: [],
   },
+  ...(process.env.CLOUDFLARE_DEPLOY === 'true' ? { output: 'export' } : {}),
 }
 
 module.exports = nextConfig
