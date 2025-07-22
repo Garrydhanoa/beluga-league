81838ad2de33d08751497eeb859481c907a4ed53
Garrydhanoa <garryd2@educbe.ca>
2025-07-22 14:32:36 -0600

    Refactor globals.css and next.config.js; streamline image configuration and enhance navigation styles

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: [
-      'cdn.wallpapersafari.com',
-      'www.rocketleague.com',
-      'cdn2.unrealengine.com'
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      }
-    ],
+    unoptimized: true,
   },
 }
 
